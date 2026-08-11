a8a285cdc6b550891be28c66acceafbe02953141
Kent Slaymaker <kentslaymaker@gmail.com>
2026-08-10 23:08:40 -0600

    loggin specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  # before_action :logged_in, except: [:new, :create]
+  before_action :logged_in, except: [:new, :create, :index, :show]
 
   # GET /users
   def index
