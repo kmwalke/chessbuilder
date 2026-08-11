a29903a9092c9bce084b7df352eb4d16c4c7e703
Kent Slaymaker <kentslaymaker@gmail.com>
2026-08-10 23:00:10 -0600

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,21 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :current_user, :logged_in
+
+  def current_user
+    if session[:user_id]
+      @user ||= User.find(session[:user_id])
+    else
+      @user = nil
+    end
+  end
+
+  def logged_in
+    return unless current_user.nil?
+
+    session[:orig_destination] = request.path
+    redirect_to login_path
+  end
 end
---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Sessions' do
+  describe 'logged out' do
+    it 'redirects to login page' do
+      skip('use some private page here')
+      visit root_path
+      expect(page).to have_current_path(login_path, ignore_query: true)
+    end
+  end
+
+  describe 'logged in' do
+    before do
+      login
+    end
+
+    it 'logs in' do
+      expect(page).to have_text('Log Out')
+    end
+
+    it 'logs out' do
+      logout
+      expect(page).to have_current_path(root_path, ignore_query: true)
+    end
+  end
+end
