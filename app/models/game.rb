9af18617c8560d32e2de3af35520f85794a9f48e
Kent Slaymaker <kentslaymaker@gmail.com>
2026-08-12 16:46:48 -0600

    game names

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,6 @@ class Game < ApplicationRecord
   belongs_to :guest, class_name: 'User'
 
   def name
-    "#{host.name} VS #{guest.name}"
+    "#{host.name} VS #{guest.name} - #{created_at.to_fs(:long_ordinal)}"
   end
 end
