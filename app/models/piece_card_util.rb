6f2bb1d1183cfbdde966e9fd986e616d5268245f
Kent Slaymaker <kentslaymaker@gmail.com>
2026-08-14 14:03:54 -0600

    formatting

--- app/models/piece_card_util.rb
+++ app/models/piece_card_util.rb
@@ -14,17 +14,32 @@ class PieceCardUtil < ApplicationRecord
   def self.card_data
     [
       {
-        name: PieceCard::PAWN,
-        level: 0,
-        host_symbol: '♙',
-        guest_symbol: '♟',
-        rules: { start: %w[a2 b2 c2 d2 e2 f2 g2 h2] }
+        name: PieceCard::PAWN, level: 0, host_symbol: '♙', guest_symbol: '♟',
+        rules: {
+          start: %w[a2 b2 c2 d2 e2 f2 g2 h2]
+        }
       },
-      { name: PieceCard::ROOK, level: 0, host_symbol: '♖', guest_symbol: '♜', rules: { start: %w[a1 h1] } },
-      { name: PieceCard::KNIGHT, level: 0, host_symbol: '♘', guest_symbol: '♞', rules: { start: %w[b1 g1] } },
-      { name: PieceCard::BISHOP, level: 0, host_symbol: '♗', guest_symbol: '♝', rules: { start: %w[c1 f1] } },
-      { name: PieceCard::QUEEN, level: 0, host_symbol: '♕', guest_symbol: '♛', rules: { start: ['d1'] } },
-      { name: PieceCard::KING, level: 0, host_symbol: '♔', guest_symbol: '♚', rules: { start: ['e1'] } }
+      { name: PieceCard::ROOK, level: 0, host_symbol: '♖', guest_symbol: '♜',
+        rules: {
+          start: %w[a1 h1]
+        } },
+      { name: PieceCard::KNIGHT, level: 0, host_symbol: '♘', guest_symbol: '♞',
+        rules: {
+          start: %w[b1 g1],
+          moves: [[]]
+        } },
+      { name: PieceCard::BISHOP, level: 0, host_symbol: '♗', guest_symbol: '♝',
+        rules: {
+          start: %w[c1 f1]
+        } },
+      { name: PieceCard::QUEEN, level: 0, host_symbol: '♕', guest_symbol: '♛',
+        rules: {
+          start: ['d1']
+        } },
+      { name: PieceCard::KING, level: 0, host_symbol: '♔', guest_symbol: '♚',
+        rules: {
+          start: ['e1']
+        } }
     ]
   end
 end
