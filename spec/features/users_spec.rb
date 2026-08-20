6a8c0339b68c64624911921665fb42df54570ceb
Kent Slaymaker <kentslaymaker@gmail.com>
2026-08-20 16:47:09 -0600

    new specs

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -9,6 +9,25 @@ RSpec.feature 'Users' do
       visit users_path
     end
 
+    describe 'bad form data' do
+      before do
+        click_link 'New user'
+        click_button 'Create User'
+      end
+
+      it 'requires name' do
+        expect(page).to have_text('Name can\'t be blank')
+      end
+
+      it 'requires email' do
+        expect(page).to have_text('Email can\'t be blank')
+      end
+
+      it 'requires password' do
+        expect(page).to have_text('Password can\'t be blank')
+      end
+    end
+
     describe 'creates a user' do
       let(:new_user) { build(:user) }
 
