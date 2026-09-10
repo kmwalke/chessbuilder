dea821a89e29de00884e1ebe1e64f66f4f06de5b
Kent Slaymaker <kentslaymaker@gmail.com>
2026-09-10 15:49:33 -0600

    remove TODO.  disagree, who wrote this shit?

--- app/models/piece_card.rb
+++ app/models/piece_card.rb
@@ -2,7 +2,6 @@ class PieceCard < ApplicationRecord
   has_and_belongs_to_many :decks
   has_many :pieces
 
-  # TODO: each kind of piece should be a class.  The rules defined in PieceCardUtil should be defined in the class
   PAWN   = 'Pawn'.freeze
   ROOK   = 'Rook'.freeze
   KNIGHT = 'Knight'.freeze
