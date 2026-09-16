e28c38c1e4946b5b75857e8e6bf1e3b09477971d
Kent Slaymaker <kentslaymaker@gmail.com>
2026-09-15 18:33:35 -0600

    start move specs

--- db/migrate/20260915182813_add_has_moved_to_pieces.rb
+++ db/migrate/20260915182813_add_has_moved_to_pieces.rb
@@ -3,12 +3,11 @@ class AddHasMovedToPieces < ActiveRecord::Migration[8.1]
     add_column :pieces, :has_moved?, :boolean, null: false, default: false
 
     # at this point, only pawns have a start_vector
-    Piece.joins(:piece_card).where(piece_card: {name: PieceCard::PAWN}).each do |pawn|
+    Piece.joins(:piece_card).where(piece_card: { name: PieceCard::PAWN }).find_each do |pawn|
       next unless pawn.rules['start'].include?(pawn.position)
 
       pawn.update(has_moved?: true)
     end
-
   end
 
   def down
--- spec/requests/rule_spec.rb
+++ spec/requests/rule_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
+require './spec/requests/requests_helper'
 
-RSpec.feature 'Rules' do
+RSpec.describe 'Rules' do
   let!(:user1) { create(:user) }
   let!(:user2) { create(:user) }
 
@@ -12,16 +13,32 @@ RSpec.feature 'Rules' do
       visit games_path
     end
 
-    it 'test generic chess rules here' do
-      skip('list ideas here')
+    describe 'test generic chess rules here', skip: 'working on it' do
       # TODO: Check that enemy pieces block travel, but can be captured
       # TODO: Check that friendly pieces block travel, but can't be captured
       # TODO: Diagonal attacks from pawns
       # TODO: En passante, dear god
+      # TODO: Castling???  Dual piece movement!!! :barf
       # TODO: Pawns moving twice at the opening
       # TODO: Check for other chess rules
       # TODO: Break up in sections for each piece?
       # TODO: Get original pieces up and running fully before making new pieces
+
+      describe 'pawns' do
+        it 'moves 2 spaces at beginning' do
+          expect(game.valid_moves(game.pieces.find_by(position: 'd2'))).to eq(%w[d3 d4])
+        end
+
+        it 'moves 1 space after first movement' do
+          move_piece(game, 'd2d4')
+        end
+
+        it 'attack diagonally' do
+        end
+
+        it 'can\'t attack straight ahead' do
+        end
+      end
     end
   end
 end
