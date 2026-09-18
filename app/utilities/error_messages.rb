526bcbc1e0455471890f8d7cc1d1796dda58f32a
Kent Slaymaker <kentslaymaker@gmail.com>
2026-09-18 09:44:05 -0600

    generic error

---
+++ app/utilities/error_messages.rb
@@ -0,0 +1,12 @@
+class ErrorMessages
+  BAD_INPUT = {
+    select_piece: 'You must select a piece to move.',
+    select_move: 'You must select a move for your piece'
+  }.freeze
+  CHEATING  = {
+    request_spoofing: 'Nice try. You have skills.  Email kentslaymaker@gmail.com to contribute.'
+  }.freeze
+  GENERIC = {
+    error: 'An error has occured.'
+  }
+end
